a632a2c19ba7a9bb195ce5a02554ca9aab41e4e2
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2024-02-01 16:23:33 +0100

    Aggiungere i file di progetto.

---
+++ BE-U1-W1-D4-Esercitazione/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D4_Esercitazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool esci = false;
+
+            while (!esci) 
+            {
+                Console.WriteLine("==========OPERAZIONI==========");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Logout");
+                Console.WriteLine("3. Verifica ora e data di login");
+                Console.WriteLine("4. Lista degli accessi");
+                Console.WriteLine("5. Esci");
+
+                Console.WriteLine("=========================");
+
+                Console.Write("Opzione: ");
+                int opzione =Convert.ToInt16(Console.ReadLine());
+
+                switch(opzione) 
+                {
+                    case 1:
+                        Console.Write("Username: ");
+                        string username=Console.ReadLine();
+
+                        Console.Write("Password: ");
+                        string password=Console.ReadLine();
+
+                        Console.Write("Conferma password: ");
+                        string confermaPassword = Console.ReadLine();
+
+                        Utente.Login(username, password, confermaPassword);
+                        break;
+
+                    case 2:
+                        Utente.Logout();
+                        break;
+
+                    case 3:
+                        Utente.VerificaOraDataLogin();
+                        break;
+
+                    case 4:
+                        break;
+                    case 5:
+                        esci = true;
+                        break;
+
+                    default:
+                    break;
+
+
+                
+                }
+            }
+
+        }
+    }
+}
---
+++ BE-U1-W1-D4-Esercitazione/Utente.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D4_Esercitazione
+{
+    public static class Utente
+    {
+        private static string _username;
+        private static string _password;
+        private static bool _isLoggedIn;
+        private static List<string> _logins;
+
+        static Utente()
+        {
+           _logins = new List<string>();
+        }
+
+        public static string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public static string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public static bool IsLoggedIn
+        {
+            get { return _isLoggedIn; }
+        }
+
+        public static void Login(string username, string password, string confermaPassword)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                Console.WriteLine("Inserisci una username valida.");
+                return;
+            }
+
+            if (password == confermaPassword)
+            {
+                _username = username;
+                _password = password;
+                _isLoggedIn = true;
+                _logins.Add($"Login effettuato il {DateTime.Now}");
+                Console.WriteLine("Login effettuato con successo!");
+            }
+            else
+            {
+                Console.WriteLine("Le password non coincidono. Riprova.");
+            }
+        }
+
+        public static void Logout()
+        {
+            if (_isLoggedIn)
+            {
+                _isLoggedIn = false;
+                Console.WriteLine("Logout avvenuto con successo");
+            }
+            else
+            {
+                Console.WriteLine("Utente non loggato.");
+            }
+        }
+
+        public static void VerificaOraDataLogin()
+        {
+            if(_isLoggedIn)
+            {
+                Console.WriteLine("Ultimo login effettuato il " + _logins[_logins.Count - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente autenticato. Impossibile verificare l'ora e la data del login.");
+            }
+        }
+
+        public static void ListaAccessi()
+        {
+
+        }
+    }
+}
